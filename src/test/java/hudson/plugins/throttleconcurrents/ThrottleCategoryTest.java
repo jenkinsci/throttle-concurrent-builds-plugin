f2039ed4410164be5b1bded59ad532bd4bd452e4
Darren Ball <darren_ball@intuit.com>
2016-02-22 21:21:16 -0500

    Restoring whitespace - ThrottleCategoryTest.java

--- src/test/java/hudson/plugins/throttleconcurrents/ThrottleCategoryTest.java
+++ src/test/java/hudson/plugins/throttleconcurrents/ThrottleCategoryTest.java
@@ -15,7 +15,7 @@
  * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package hudson.plugins.throttleconcurrents;
-// @formatter:off
+
 import static org.junit.Assert.*;
 
 import java.util.List;
@@ -60,4 +60,3 @@ public class ThrottleCategoryTest
             expectedMax, actualMax);
     }
 }
-// @formatter:on
