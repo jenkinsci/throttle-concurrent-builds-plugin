f42db015180d8bb1917b5e439131364773d1db76
Jim Klimov <jim@jimklimov.com>
2019-06-28 19:29:48 +0200

    ThrottleQueueTaskDispatcher.java isAnotherBuildWithSameParametersRunningOnNode() : refactor paramsToCompareSize to calculate it once

--- src/main/java/hudson/plugins/throttleconcurrents/ThrottleQueueTaskDispatcher.java
+++ src/main/java/hudson/plugins/throttleconcurrents/ThrottleQueueTaskDispatcher.java
@@ -336,9 +336,10 @@ public class ThrottleQueueTaskDispatcher extends QueueTaskDispatcher {
         }
         Computer computer = node.toComputer();
         List<String> paramsToCompare = tjp.getParamsToCompare();
+        Integer paramsToCompareSize = paramsToCompare.size();
         List<ParameterValue> itemParams = getParametersFromQueueItem(item);
 
-        if (paramsToCompare.size() > 0) {
+        if (paramsToCompareSize > 0) {
             itemParams = doFilterParams(paramsToCompare, itemParams);
         }
 
@@ -351,7 +352,7 @@ public class ThrottleQueueTaskDispatcher extends QueueTaskDispatcher {
                 if (currentExecutable != null &&
                         parentTask.getOwnerTask().getName().equals(itemTaskName)) {
                     List<ParameterValue> executingUnitParams = getParametersFromWorkUnit(exec.getCurrentWorkUnit());
-                    if (paramsToCompare.size() > 0) {
+                    if (paramsToCompareSize > 0) {
                         executingUnitParams = doFilterParams(paramsToCompare, executingUnitParams);
                     }
 
